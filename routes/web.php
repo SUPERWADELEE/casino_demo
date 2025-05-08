c32c7507fce0bfb8974a7184c8dba06d89394d76
李威德 <liweide@liweidedeMacBook-Air.local>
2025-05-08 21:35:16 +0800

    feat: add mobile style

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,32 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('kg3399.index');
-});
+// Route::get('/', function () {
+//     return view('kg3399.mobile');
+// });
 
 Route::get('/mall', function () {
     return view('kg3399.mall');
 });
 
 Route::get('/error', function () {
-    return view('kg3399.error');
+    return view('casino.error');
 });
 Route::get('/activite', function () {
-    return view('kg3399.activite');
+    return view('casino.activite');
 });
+// Route::get('/mobile', function () {
+//     return view('mobile');
+// });
+
+Route::get('/', function (\Illuminate\Http\Request $request) {
+    $userAgent = $request->header('User-Agent');
+
+    if (preg_match('/iPhone|Android|Mobile/i', $userAgent)) {
+        return view('casino.mobile');
+    } else {
+        return view('casino.index');
+    }
+});
+
+
