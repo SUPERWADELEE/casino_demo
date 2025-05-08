18190c4c96782f36a5c2c716657590c358682dc5
李威德 <liweide@liweidedeMacBook-Air.local>
2025-05-09 01:57:03 +0800

    fix: fix mobile mall bottum color and pressure box display

--- resources/views/casino/index.blade.php
+++ resources/views/casino/index.blade.php
@@ -4,6 +4,75 @@
     <meta data-n-head="ssr" charset="utf-8"><meta data-n-head="ssr" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"><meta data-n-head="ssr" name="description" content="KG娛樂城十年信譽保證，菲律賓合法領牌，充值、提款十分鐘到帳。 遊戲種類齊全，優惠多多就是要你發。"><meta data-n-head="ssr" name="format-detection" content="telephone=no"><title>KG娛樂城</title><link data-n-head="ssr" rel="icon" type="image/x-icon" href="imgs_v2/300.png"><link data-n-head="ssr" rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;display=swap"><link data-n-head="ssr" rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
     
 <style>
+  @keyframes burst {
+  0% {
+    transform: scale(0.3);
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+
+.user-welfare .item.one.open {
+  animation: burstOne 0.4s ease-out forwards;
+}
+
+.user-welfare .item.two.open {
+  animation: burstTwo 0.4s ease-out forwards;
+}
+
+@keyframes burstOne {
+  0% {
+    transform: scale(0.3) translate(0, 0);
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1) translate(-50px, -100px);
+    opacity: 1;
+  }
+}
+
+@keyframes burstTwo {
+  0% {
+    transform: scale(0.3) translate(0, 0);
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1) translate(60px, -80px);
+    opacity: 1;
+  }
+}
+@keyframes burstReverse {
+  0% {
+    transform: scale(1) translate(0, 0);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(0.3) translate(0, 0);
+    opacity: 0;
+  }
+}
+
+.user-welfare .item {
+  position: absolute;
+  left: 0;
+  top: 0;
+  visibility: hidden;
+  opacity: 0;
+  transform: scale(0.3);
+}
+
+.user-welfare .item.open {
+  visibility: visible;
+  animation: burst 0.4s ease-out forwards;
+}
+
+.user-welfare .item.close {
+  visibility: visible; /* 讓動畫能看見 */
+  animation: burstReverse 0.4s ease-in forwards;
+}
   html, body {
     margin: 0;
     padding: 0;
@@ -870,5 +939,63 @@ body,html{-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;font-family:"N
 </div> <div class="q" data-v-4c75f541 onclick="window.location.href='/error'" style="cursor: pointer;"><img src="imgs_v3/2.png" alt data-v-4c75f541> <span data-v-4c75f541>APP下載</span></div> <div class="q" data-v-4c75f541 onclick="window.location.href='/error'" style="cursor: pointer;"><img src="imgs_v3/3.png" alt data-v-4c75f541> <span data-v-4c75f541>客服</span></div> <div class="not-login" data-v-4c75f541 onclick="window.location.href='/error'" style="cursor: pointer;"><div class="sign_in" data-v-4c75f541><img src="imgs_v3/5.png" alt data-v-4c75f541><span data-v-4c75f541>登入</span></div> <div class="sign_up" data-v-4c75f541 onclick="window.location.href='/error'" style="cursor: pointer;"><img src="imgs_v3/6.png" alt data-v-4c75f541><span data-v-4c75f541>註冊</span></div></div></div></div></div> <div class="pc-layout-inner" data-v-fa491fe8 data-v-7f2355b8><!----> <div class="section section1" data-v-fa491fe8><div class="section_inner" data-v-fa491fe8><canvas id="qr_code" class="qr_code" data-v-fa491fe8></canvas> <div class="marquee-section" data-v-fa491fe8><div class="marquee-wrap" data-v-9d54aee6 data-v-fa491fe8><span class="new-icon" data-v-9d54aee6><svg aria-hidden="true" data-prefix="fal" data-icon="bullhorn" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg class="svg-inline--fa fa-bullhorn fa-w-18" data-v-9d54aee6><path fill="currentColor" d="M544 192V32c0-17.7-14.3-32-32-32-65 56-174 128-304 128H48c-26.5 0-48 21.5-48 48v96c0 26.5 21.5 48 48 48h16.8c-5.3 58.6 15 107 28.9 151.6 1.5 5 4.3 9.5 8 13.2C119 502.1 142.8 512 167 512c31.8 0 60.9-17 75.5-47.7 7-14.7 1.9-32.3-11.9-40.9-27.9-17.4-32-22.5-26.8-31.5 6.9-12.1 5.2-27.3-4.2-37.6-5.3-5.8-1.3-27.4 11-34.3 128.9.9 236.9 72.4 301.5 128 17.7 0 32-14.3 32-32V256c17.7 0 32-14.3 32-32s-14.4-32-32.1-32zM213.7 450.5c-17.4 36.7-63.5 37.4-89.4 11.6C110.5 417.6 91.4 372.9 97 320h73.7c-8.3 19.2-7.6 42.1 5.3 56-19.9 34.7 8 56 37.7 74.5zM32 272v-96c0-8.8 7.2-16 16-16h176v128H48c-8.8 0-16-7.2-16-16zm480 134.4c-36.6-29.1-74.7-53.4-113.7-72.2-47.3-22.9-95-37.4-142.3-43.2V157c47.3-5.9 95-20.3 142.3-43.2 38.9-18.9 77.1-43.1 113.7-72.2v364.8z" data-v-9d54aee6></path></svg></span> <div class="message-content" data-v-9d54aee6><div id="message-wrap" class="message-wrap" data-v-9d54aee6><ul id="messageInner" class="message-innter" data-v-9d54aee6></ul></div></div></div></div></div></div> <div class="section section2" data-v-fa491fe8></div> <div class="section section3" data-v-fa491fe8></div> <div class="section section4" data-v-fa491fe8></div></div> <div class="footer" data-v-578f98da data-v-7f2355b8>
   ©2024 KG娛樂城. All Rights Reserves.
 </div> <!----> <!----> <!----></div></div></div><script>window.__NUXT__=(function(a,b,c,d,e){return {layout:"default",data:[{}],fetch:{},error:b,state:{loading:a,alert:a,isApp:a,alerObj:{type:"warning",message:"message"},userDetail:{},login:a,mailCount:0,gameList:[],gameGroup:{live:[],sport:[],chess:[],egame:[],lottery:[]},curGameType:"home",acitvityList:[],appDownBar:c,startGameDialog:a,startGameObj:{},isMobile:a,isWebp:a,appTitle:d,appSubTitle:d,usdtTeachShow:a,i18n:{routeParams:{}}},serverRendered:c,routePath:e,config:{_app:{basePath:e,assetsPath:"\u002F_nuxt\u002F",cdnURL:b}}}}(false,null,true,"","\u002F"));</script><script src="_nuxt/3e1afeb.js" defer></script><script src="_nuxt/5dba238.js" defer></script><script src="_nuxt/83930b0.js" defer></script><script src="_nuxt/e347167.js" defer></script><script src="_nuxt/c5f611e.js" defer></script><script src="_nuxt/d48f308.js" defer></script><script src="_nuxt/f906dfe.js" defer></script><script src="_nuxt/8860eed.js" defer></script>
+<script>
+window.addEventListener('DOMContentLoaded', function () {
+  const target = document.querySelector('div[draggable="true"]');
+  if (target) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+  <div id="user-welfare" class="user-welfare" data-v-7b02648a data-v-7f2355b8>
+    <div style="position: relative;" data-v-7b02648a>
+      <img width="90" src="/hot_v4.png" alt="userWelfare" id="toggleBtn" data-v-7b02648a>
+      <img width="90" src="/float_icon.png" alt="welfare" class="item one" id="imgOne" data-v-7b02648a onclick="window.location.href='/error'" />
+      <img width="90" src="/share_friend.png" alt="welfare" class="item two" id="imgTwo" data-v-7b02648a onclick="window.location.href='/error'" />
+      
+    </div>
+  </div>
+`;
+    target.appendChild(div);
+
+    // 控制點擊事件
+    const toggleBtn = document.getElementById('toggleBtn');
+    const imgOne = document.getElementById('imgOne');
+    const imgTwo = document.getElementById('imgTwo');
+
+    let shown = false;
+    toggleBtn.addEventListener('click', () => {
+  if (!shown) {
+  // 開啟動畫（補上 visibility）
+  imgOne.style.visibility = 'visible';
+  imgTwo.style.visibility = 'visible';
+
+  imgOne.classList.remove('close');
+  imgTwo.classList.remove('close');
+  imgOne.classList.add('open');
+  imgTwo.classList.add('open');
+} else {
+  // 關閉動畫
+  imgOne.classList.remove('open');
+  imgTwo.classList.remove('open');
+  imgOne.classList.add('close');
+  imgTwo.classList.add('close');
+}
+  shown = !shown;
+  imgOne.addEventListener('animationend', () => {
+  if (imgOne.classList.contains('close')) {
+    imgOne.classList.remove('close');
+    imgOne.style.visibility = 'hidden'; // 可選，收回後完全隱藏
+  }
+});
+
+imgTwo.addEventListener('animationend', () => {
+  if (imgTwo.classList.contains('close')) {
+    imgTwo.classList.remove('close');
+    imgTwo.style.visibility = 'hidden';
+  }
+});
+});
+  }
+});
+</script>
   </body>
 </html>
