d4672924777b47409b866c9c9edf2c6562ce2af6
李威德 <liweide@liweidedeMacBook-Air.local>
2025-05-08 10:28:51 +0800

    feat:remove usdt and recover index page navigate icon

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/index', function () {
     return view('kg3399.index');
 });
 
