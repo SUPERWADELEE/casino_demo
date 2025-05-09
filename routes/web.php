deb63a765593852f014954db15164e03e23c3beb
李威德 <liweide@liweidedeMacBook-Air.local>
2025-05-09 08:20:14 +0800

    feat: add mobile mall version

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 
 
+Route::get('/test', function () {
+    return view('casino.test_mall');
+});
 
-Route::get('/mall', function () {
-    return view('casino.mall');
+Route::get('/mall', function (\Illuminate\Http\Request $request) {
+    $userAgent = $request->header('User-Agent');
+
+    if (preg_match('/iPhone|Android|Mobile/i', $userAgent)) {
+        return view('casino.mobile_mall');
+    } else {
+        return view('casino.mall');
+    }
 });
 
 Route::get('/error', function () {
